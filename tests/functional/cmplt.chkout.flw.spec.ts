e6b94e70707af111cb7f023e1d0892d082089bf6
AnuAknow <everettaknowledge50@gmail.com>
2026-05-26 22:17:22 -0700

    initial check-in of cmplt.chkout.flw.spec.ts Spec and package jspn

---
+++ tests/functional/cmplt.chkout.flw.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Should complete the checkout flow', () => {
+
+    test.beforeEach("Login", async ({ page }) => {
+        await page.goto('https://www.saucedemo.com/');
+        await page.locator('[data-test="username"]').click();
+        await page.locator('[data-test="username"]').click();
+        await page.locator('[data-test="username"]').fill('standard_user');
+        await page.locator('[data-test="password"]').click();
+        await page.locator('[data-test="password"]').fill('secret_sauce');
+        await page.locator('[data-test="login-button"]').click();
+    });
+
+    // test("Select Item and Checkout ", async ({ page }) => {
+
+    // });
+
+});
